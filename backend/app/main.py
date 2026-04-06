e2e5a24ed20b5c8a467c300206a0d8d943c56486
ashklucy5 <ashklucy5@gmail.com>
2026-04-06 11:44:26 +0800

    update cros

--- backend/app/main.py
+++ backend/app/main.py
@@ -106,8 +106,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000",
+        "https://task-manager-rho-six-58.vercel.app"
         "*",
     ],
     allow_credentials=True,
