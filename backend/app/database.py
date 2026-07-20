caa5f9304aaba5cb925c5228f2e2d81d78ef81e6
ashklucy5 <ashklucy5@gmail.com>
2026-07-21 02:36:10 +0800

    update time stamp

--- backend/app/database.py
+++ backend/app/database.py
@@ -9,7 +9,8 @@ engine = create_engine(
     echo=settings.DEBUG,
     pool_pre_ping=True,
     pool_size=10,
-    max_overflow=20
+    max_overflow=20,
+    connect_args={"connect_timeout": 10}
 )
 
 # Create session factory
